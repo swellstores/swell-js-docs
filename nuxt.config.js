557ee99fcd650d640330c0f39866dffaaa3bcdae
Dave Loneragan <dave@papertokyo.com>
2020-11-27 15:20:08 -0600

    Add hashlink fix
    https://github.com/nuxt/content/issues/376#issuecomment-702193217

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,5 +3,10 @@ import theme from '@nuxt/content-theme-docs'
 export default theme({
   router: {
     base: '/docs/js/'
+  },
+  hooks: {
+    'vue-renderer:ssr:templateParams': function(params) {
+      params.HEAD = params.HEAD.replace('<base href="/docs/js/">', '')
+    }
   }
 })
